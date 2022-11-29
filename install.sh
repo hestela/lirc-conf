e0bf389471b7ba7b810f6ba9624893e76e69ed7b
Henry Estela <henryestela@gmail.com>
2022-11-28 20:31:14 -0800

    remove modules

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ cp benq/* $lirc/benq
 cp kenwood/* $lirc/kenwood
 cp lircd.conf /etc/lirc/
 
-echo "lirc_dev" >> /etc/modules
-echo "lirc_rpi gpio_in_pin=18 gpio_out_pin=17" >> /etc/modules
+#echo "lirc_dev" >> /etc/modules
+#echo "lirc_rpi gpio_in_pin=18 gpio_out_pin=17" >> /etc/modules
